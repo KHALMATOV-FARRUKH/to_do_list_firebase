10d189c2e0b1157965bf26b46b26635ac9faf818
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-04-14 17:10:30 +0500

    Created main_screen.dart
    Added:
    google-services.json
    GoogleService-Info.plist
    
    Added dependencies:
    cloud_firestore:
    firebase_core:
    
    classpath 'com.google.gms:google-services:4.3.15'
    apply plugin: 'com.google.gms.google-services'
    implementation platform('com.google.firebase:firebase-bom:31.3.0')

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list_firebase/pages/home_page.dart';
+import 'package:to_do_list_firebase/pages/main_screen.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
-      home: const HomePage(),
+      theme: ThemeData(
+        primaryColor: Colors.deepOrangeAccent,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MainScreen(),
+        '/todo': (context) => const HomePage()
+      },
     ));
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,11 +10,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late String _userToDo;
   List todoList = [];
+
+  void initFirebase()async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  }
 
   @override
   void initState() {
     super.initState();
+
+    initFirebase();
+
     todoList.addAll([
       "Buy milk",
       "Buy meat",
@@ -24,6 +35,33 @@ class _HomePageState extends State<HomePage> {
     ]);
   }
 
+  void _menuOpen() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrangeAccent,
+          title: const Text("Menu"),
+          // centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.pushNamedAndRemoveUntil(
+                    context, '/', (route) => false);
+              },
+              child: const Text("На главную"),
+            ),
+            const SizedBox(width: 15),
+            const Text("Наше простое меню"),
+          ],
+        ),
+      );
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,26 +70,73 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.deepOrangeAccent,
         title: const Text("To Do List"),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: _menuOpen,
+            icon: Icon(Icons.menu_outlined),
+          ),
+        ],
       ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Dismissible(
-            key: Key(todoList[index]),
-            child: Card(
-              child: ListTile(
-                leading: Icon(Icons.mode_edit_outline),
-                trailing: Icon(Icons.delete_sweep, color: Colors.red),
-                title: Text(todoList[index]),
-              ),
-            ),
-            onDismissed: (direction) {
-              setState(() {
-                todoList.removeAt(index);
-              });
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('items').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+          if(!snapshot.hasData) return Text("No records");
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Dismissible(
+                key: Key(snapshot.data!.docs[index].id),
+                child: Card(
+                  child: ListTile(
+                    // leading: Icon(Icons.mode_edit_outline),
+
+                    trailing: IconButton(
+                      icon: const Icon(
+                        Icons.delete_sweep_sharp,
+                        color: Colors.deepOrangeAccent,
+                      ),
+                      onPressed: () {
+                        FirebaseFirestore.instance.collection('items').doc(snapshot.data!.docs[index].id).delete();
+                      },
+                    ),
+                    title: Text(snapshot.data!.docs[index].get('itemTitle')),
+                  ),
+                ),
+                onDismissed: (direction) {
+                  FirebaseFirestore.instance.collection('items').doc(snapshot.data!.docs[index].id).delete();
+                },
+              );
             },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepOrangeAccent,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Add a note"),
+                content: TextField(
+                  onChanged: (String value) {
+                    _userToDo = value;
+                  },
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: () {
+                      FirebaseFirestore.instance.collection('items').add({'itemTitle': _userToDo});
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("Add"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Icon(Icons.edit),
       ),
     );
   }
